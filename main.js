99b75694f74f5386419223639804129c6e4a170b
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2024-03-24 13:06:52 +0100

    Version Etape 4.1 - Initialisation des données et du codage - liste de rendu

--- main.js
+++ main.js
@@ -3,10 +3,8 @@ const app = Vue.createApp({
         return {
            product: 'Socks',
            image: './assets/images/socks_green.jpg',
-           inventory : 100,
-           // Challenge
-           onSale: true
-           // Challenge
+           inStock: true,
+           details: ['50% cotton', '30% wool', '20% polyester']
         }
     },
 })
